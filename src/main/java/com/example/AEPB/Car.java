b8ef0780ebc4ce765ef502cc9dd89d40ba3f0017
Fang Wang <sjsSJS0522@@>
2022-09-21 15:48:05 +0800

    "feat: ParkingLot"

---
+++ src/main/java/com/example/AEPB/Car.java
@@ -0,0 +1,14 @@
+package com.example.AEPB;
+
+public class Car {
+
+    private String plateNum;
+
+    public Car(String plateNum) {
+        this.plateNum = plateNum;
+    }
+
+    public String getPlateNum() {
+        return plateNum;
+    }
+}
