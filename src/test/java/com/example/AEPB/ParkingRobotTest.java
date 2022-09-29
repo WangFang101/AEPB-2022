0f6c6f7b721bc0c51265f138d1d4de280f5f0702
Fang Wang <sjsSJS0522@@>
2022-09-29 12:05:34 +0800

    [fangwang&lixiaochun] test:should_park_A_when_park_given_A_B_vacancy_rate_eq_and_gt_C

--- src/test/java/com/example/AEPB/ParkingRobotTest.java
+++ src/test/java/com/example/AEPB/ParkingRobotTest.java
@@ -23,6 +23,23 @@ class ParkingRobotTest {
         assertNotNull(car);
     }
 
+    @Test
+    void should_park_A_when_park_given_A_B_vacancy_rate_eq_and_gt_C() {
+        int size = 5;
+        ParkingLot parkingLotA = new ParkingLot(size);
+        ParkingLot parkingLotB = new ParkingLot(size);
+        ParkingLot parkingLotC = new ParkingLot(size);
+        ParkingRobot parkingRobot = new ParkingRobot(parkingLotA, parkingLotB, parkingLotC);
+        parkingLotC.park(new Car("BCDEFG"));
+        parkingLotC.park(new Car("CDEFGH"));
+
+        Ticket ticket = parkingRobot.park(new Car("ABCDEF"));
+        Car car = parkingLotA.pick(ticket);
+
+        assertNotNull(car);
+    }
+
+
 
 
 }
